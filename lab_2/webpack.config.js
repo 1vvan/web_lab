1308954eab9db0b1f1655821e30541f7f094a057
1vvan <krasnikov09122004@gmail.com>
2024-09-15 13:04:02 +0300

    lab_2

---
+++ lab_2/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/pages/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'about.html',
+      template: './src/pages/about.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+  },
+};
